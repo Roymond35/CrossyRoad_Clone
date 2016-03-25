2b8ecc61cca1bef15831a1be3db2b32279f1910c
Roy Gero <roy.gero.ii@gmail.com>
2016-03-25 10:24:03 -0400

    Refactoring the Road Prefab to be more like the Water Prefab.
    Next step is to start adding buildings to both sides.

--- Assets/Scripts/CarControl.cs
+++ Assets/Scripts/CarControl.cs
@@ -3,26 +3,23 @@ using System.Collections;
 
 public class CarControl : MonoBehaviour {
 
+	public bool startedLeft;
 	public float speed;
-	public float carSpeed;
-	public bool started = false;
-	public bool movingLeft;
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		if (started) {
-			if (movingLeft) {
-				this.transform.position = new Vector3 (this.transform.position.x + speed, this.transform.position.y, this.transform.position.z + carSpeed);
-			} else {
-				this.transform.position = new Vector3 (this.transform.position.x + speed, this.transform.position.y, this.transform.position.z - carSpeed);
-			}
+		if (startedLeft) {
+			this.transform.position = new Vector3 (this.transform.parent.position.x, this.transform.position.y, this.transform.position.z + speed);
+		} else {
+			this.transform.position = new Vector3 (this.transform.parent.position.x, this.transform.position.y, this.transform.position.z - speed);
 		}
-		if (this.transform.position.z > 6 || this.transform.position.z < -6) {
+
+		if (this.transform.position.z > 7 || this.transform.position.z < -7) {
 			Destroy (this.gameObject);
 		}
 	}
--- Assets/Scripts/SpawnCar.cs
+++ Assets/Scripts/SpawnCar.cs
@@ -3,42 +3,55 @@ using System.Collections;
 
 public class SpawnCar : MonoBehaviour {
 
-	public GameObject carPrefab;
-	public Vector3 spawnPosition;
+	public bool startLeft;
+	public float MAX_SPEED; //Remember this is not inclusive.
+	public float MIN_SPEED; 
+	public int maxSpawns;
 	public float timeDelay;
 	public float ellapsedTime;
-	public bool movingLeft;
-	public float carSpeed;
-	public const int MAX_CARS = 3;
+	public GameObject carPrefab;
+	private Vector3 prefabPosition;
+	private float zCoord;
+	public Vector3 spawnPosition;
+	private float speed;
 
 
 	// Use this for initialization
 	void Start () {
+		//Default Values in case nothing gets set.
+		MAX_SPEED = 0.1f;
+		MIN_SPEED = 0.05f;
+		maxSpawns = 3;
+
+		startLeft = Random.value > 0.5f;
+		if (startLeft) {
+			spawnPosition = new Vector3 (-7.0f, 1f, -6.0f);
+		} else {
+			spawnPosition = new Vector3 (-7.0f, 1f, 6.0f);
+		}
 		ellapsedTime = 0;
 		timeDelay = Random.Range (1f, 2.5f);
-		carSpeed = Random.Range (0.01f, 0.09f);
+		speed = Random.Range (MIN_SPEED, MAX_SPEED);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		int numberCars = this.transform.childCount;
+		int numCars = this.transform.childCount;
 		ellapsedTime += Time.deltaTime;
-		spawnPosition = new Vector3 (this.transform.position.x, spawnPosition.y, spawnPosition.z);
-		if (ellapsedTime > timeDelay && numberCars < MAX_CARS) {
+		if (ellapsedTime > timeDelay && numCars < maxSpawns) {
 			ellapsedTime = 0;
 			GameObject newBlock = Instantiate (carPrefab) as GameObject;
 			newBlock.transform.position = spawnPosition;
-			//newBlock.GetComponent<CarControl> ().speed = .03f;
-			newBlock.GetComponent<CarControl> ().carSpeed = this.carSpeed;
-			newBlock.GetComponent<CarControl> ().movingLeft = this.movingLeft;
-			if (!movingLeft) {
+			newBlock.GetComponent<CarControl> ().startedLeft = this.startLeft;
+			if (!startLeft) {
 				Vector3 rot = newBlock.transform.rotation.eulerAngles;
-				rot = new Vector3(rot.x,rot.y+180,rot.z);
-				newBlock.transform.rotation = Quaternion.Euler(rot);
+				rot = new Vector3 (rot.x, rot.y + 180, rot.z);
+				newBlock.transform.rotation = Quaternion.Euler (rot);
 			}
-			newBlock.GetComponent<CarControl> ().started = true;
+			newBlock.GetComponent<CarControl> ().speed = this.speed;
 			newBlock.transform.parent = this.transform;
 		}
+
 	}
 
 	void OnCollisionEnter(Collision c){
